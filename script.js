8ff6af87242b82aed42daf78252e7eaf7734b61f
AvatarX678 <toramlife40@gmail.com>
2026-07-21 19:20:22 +0700

    update

--- script.js
+++ script.js
@@ -75,6 +75,9 @@ const buttonoperation = document.querySelectorAll(".operation");
             return
          }
          operationloop()
+         if(terkunci){
+            return
+         }
          input=input+this.value;
          number= ""
 
@@ -126,7 +129,7 @@ function operation(){
       reset(result)
    }
    else{
-      if(number2 == "0"){
+      if(Number(number2)== 0){
          angka.textContent="gak bisa bagi dengan 0"
          terkunci = true
          return
